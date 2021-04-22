856d42e7cd4084a4e8ce7a87cec981571a39520d
Mohamed AbdelSattar <mohamed.abdelsattar92@hotmail.com>
2021-04-22 15:12:24 +0200

    Using network interface id instead of instance id to get security groups

--- security_groups_reducer.py
+++ security_groups_reducer.py
@@ -6,10 +6,16 @@ import boto3
 '''
 AWS Security Groups Analyzer/Reducer helps you see the overall view of your EC2 instance security groups,
 which ports are open to what IP ranges.
+
+Features:
+1- give you a summary of the security groups that are attached to your instance or rds database
+2- (if possible) analyze floating security groups and return their IDs to delete them by ID
+    2.1- using the information in the following link: https://aws.amazon.com/premiumsupport/knowledge-center/ec2-find-security-group-resources/ I can get what is
+        using my security group and can delete it or get some details of what is using it.
 '''
 def parse_command_line_arguments():
     args_parser = argparse.ArgumentParser(description='Process script arguments')
-    args_parser.add_argument('-i', '--instance-id', help='The instance id that you need to investigate its security groups')
+    args_parser.add_argument('-n', '--network-interface-id', help='The network interface id that you need to investigate its security groups')
     args_parser.add_argument('-r', '--region', help='The AWS region where the instance resides')
     args_parser.add_argument('-p', '--profile', help='The AWS named profile you wish to use with this script')
     args_parser.add_argument('-d', '--detail', help='The Details of each security group with Inbound and Outbound rules')
@@ -20,7 +26,6 @@ def configure_boto3_session(profile_name=''):
     if profile_name:
         return boto3.session.Session(profile_name=profile_name)
     return boto3.session.Session()
-
 
 def is_all_traffic_allowed(permission_entry):
     return permission_entry['IpProtocol'] == '-1'
@@ -41,18 +46,18 @@ def process_permission_entry(permission_entry):
 global_start = time()
 args = parse_command_line_arguments()
 
-instance_id = args.instance_id
+network_interface_id = args.network_interface_id
 region = args.region
 profile = args.profile
 
 boto3_session = configure_boto3_session(profile)
 
 ec2_resource = boto3_session.resource('ec2', region_name=region)
-aws_instance = ec2_resource.Instance(instance_id)
+network_interface = ec2_resource.NetworkInterface(network_interface_id)
 
 
 security_groups_list = []
-for security_group in aws_instance.security_groups:
+for security_group in network_interface.groups:
     security_groups_list.append(ec2_resource.SecurityGroup(security_group['GroupId']))
 
 # print(security_groups_list)
